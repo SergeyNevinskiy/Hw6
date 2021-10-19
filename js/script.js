b2550fc512861768acfeb9a6d4aba29e9153b772
SergeyNevinskiy <88575089+SergeyNevinskiy@users.noreply.github.com>
2021-10-19 11:05:17 +0300

    Hw6

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+// Number 2 
+// const result = Boolean(Number("10")) + 1;
+// console.log(result, typeof result);
+
+// sub0 string
+// const result = "sub"  + Number(false);
+// console.log(result, typeof result);
+
+
+// 1456 Number
+// const result = 16  *  "      91    ";
+// console.log(result, typeof result);
+
+
+// -69 Number
+// const result = true-70;
+// console.log(result, typeof result);
+
+
+// 12 Number
+// const result = Number(1 + String(1)) + 1;
+// console.log(result, typeof result);
+
+
+// const secondInHour = (60 * 60);
+// const result = secondInHour * +prompt("Введите количество часов");
+// alert(`Итого : ${result} секунд`);
+
+
+// let num = 1;
+// num += 12;
+// num -= 14;
+// num *= 5; 
+// num /= 7;
+// num++;
+// num--;
+// alert(num);
